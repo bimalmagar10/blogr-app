3d4b4ca95afc4ba7d4c4705c5c95e73b74fe75a6
bimalmagar10 <bimalmagar873@gmail.com>
2021-03-28 21:17:44 +0545

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+(() => {
+	const modify = (e) => {
+		e.target.classList.toggle("rotate");
+		let el = e.target;
+		el.closest(".header__nav-list").children[1].classList.toggle("opacity");
+		// alert(el.closest(".header__nav-list").children[1]);
+	};
+	Array.from(document.querySelectorAll("#arrow-icon")).forEach(el => {
+		el.addEventListener("click",modify)
+	});
+	const showMobileNav = (e) => {
+		e.preventDefault();
+		let imageName = e.target.getAttribute("src");
+		if(imageName !== "../images/icon-close.svg") {
+			e.target.setAttribute("src","../images/icon-close.svg");
+		} else {
+			e.target.setAttribute("src","../images/icon-hamburger.svg");
+		}
+		let mobileNavElement = document.querySelector(".mobile-nav");
+		mobileNavElement.classList.toggle("show-mobile-nav");
+	};
+	const showMobileNavItems = (e) => {
+		if(e.target.id === "js--mobile-nav"){
+			let target = e.target;
+			target.classList.toggle("rotate");
+			target.parentNode.children[0].classList.toggle("toggle-color");
+			target.closest(".mobile-nav__list").children[1].classList.toggle("show-mobile-nav-item");
+		}
+	};
+	document.querySelector(".nav-hamburger").addEventListener("click",showMobileNav);
+	// Array.from(document.querySelector(".mobile-nav__list")).forEach(el => {
+	// 	el.addEventListener("click",showMobileNavItems);
+	// })
+	document.querySelector(".mobile-nav").addEventListener("click",showMobileNavItems);
+
+	$("#animation-1").waypoint((direction) => {
+          $("#animation-1").addClass("animate__animated animate__fadeInUp");
+	},
+	{
+		offset:"500px"
+	});
+	
+})();
